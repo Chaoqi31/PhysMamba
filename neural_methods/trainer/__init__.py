601bd3a8039b3a0282c1f1924af941bbc6253f09
Chaoqi <136579807+Chaoqi31@users.noreply.github.com>
2024-09-20 00:16:36 +0800

    Add files via upload

---
+++ neural_methods/trainer/__init__.py
@@ -0,0 +1,8 @@
+import neural_methods.trainer.BaseTrainer
+import neural_methods.trainer.PhysnetTrainer
+import neural_methods.trainer.TscanTrainer
+import neural_methods.trainer.DeepPhysTrainer
+import neural_methods.trainer.EfficientPhysTrainer
+import neural_methods.trainer.BigSmallTrainer
+import neural_methods.trainer.PhysFormerTrainer
+import neural_methods.trainer.PhysMambaTrainer
